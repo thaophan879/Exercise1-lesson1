fe34946311ecb00a088a30213e6f44170e3551a6
thaophan879 <thao.phan080799@gmail.com>
2024-01-06 17:36:36 +0700

    init

---
+++ tests/example.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://www.guru99.com/xpath-selenium.html');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/XPath in Selenium/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://www.guru99.com/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Selenium' }).first().click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Selenium Tutorial Syllabus' })).toBeVisible();
+});
